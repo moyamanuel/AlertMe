b8dc595b8777449c9a2fd492ee6a8a4ed7263d4e
Manuel Moya <moya.manuel41@gmail.com>
2019-01-20 02:10:42 -0800

    Setup MySQL database in project

--- cruzhacks2019/cruzhacks2019/settings.py
+++ cruzhacks2019/cruzhacks2019/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'cruzhacks2019.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'test',
+        'USER': 'test',
+        'PASSWORD': 'Testing123!@#',
+        'HOST': '104.236.37.73',
+        'PORT': '3306',
     }
 }
 
