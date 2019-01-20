a5d0f265a45fc2daa93a4127571d2d575a5de035
Manuel Moya <moya.manuel41@gmail.com>
2019-01-20 08:19:10 -0800

    Add Person model and write view to collect user data

--- cruzhacks2019/backend/views.py
+++ cruzhacks2019/backend/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from backend.models import Person
 
 # Create your views here.
 
 def index(request):
+    if request.method == "POST":
+        new_subscriber = Person(
+            first_name = request.POST.get('first'),
+            last_name = request.POST.get('last'),
+            phone_number = request.POST.get('number')
+        )
+        new_subscriber.save()
     return render(request, "backend/index.html")
